1d8f0522d69fbf6730e4ac3e5d13eb507ad8c835
MateoIr <112603854+moxolito@users.noreply.github.com>
2024-05-18 13:27:57 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,5 @@
+const Register = () => {
+  return <div>Resgister</div>;
+};
+
+export default Register;
