dfcb3d77cfca0e0ef9bf27216bbca814bb842e5b
MateoIr <mateoiriso@gmail.com>
2024-12-14 12:29:09 -0300

    turnocon detalle en paseo

--- src/pages/turnRegister/Calendar.jsx
+++ src/pages/turnRegister/Calendar.jsx
@@ -5,8 +5,8 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import "./Calendar.css"; // Importar estilos para el modal y el calendario
 import useGetDaycareServices from "../../hooks/turn/useGetDaycareServices"; // Hook para obtener los eventos
-import { Link } from 'react-router-dom';
-import esLocale from "@fullcalendar/core/locales/es"
+import { Link } from "react-router-dom";
+import esLocale from "@fullcalendar/core/locales/es";
 import CustomButton from "../../components/customButton/CustomButton";
 import { Box, Grid } from "@mui/material";
 
@@ -14,9 +14,9 @@ const Calendar = () => {
   const [selectedEvent, setSelectedEvent] = useState(null);
   const [isModalOpen, setModalOpen] = useState(false);
 
-// Obtener los eventos desde el hook
-const { turnos, isLoading, error } = useGetDaycareServices(); // Llamada al API
-/*const paseos=[
+  // Obtener los eventos desde el hook
+  const { turnos, isLoading, error } = useGetDaycareServices(); // Llamada al API
+  /*const paseos=[
       {"id": 2,
       "fechaturno": "2024-11-22",
       "idmascota":2},
@@ -69,41 +69,66 @@ const transformedEvents = turnos && turnos.length > 0 && paseos && paseos.length
 
     */
 
-
-// Transformar los datos del API al formato que FullCalendar necesita
-//console.log('Eventos API:', turnos);
-const transformEvents = (turnos) => {
-  return turnos.map((event) => ({
+  // Transformar los datos del API al formato que FullCalendar necesita
+  //console.log('Eventos API:', turnos);
+  const transformEvents = (turnos) => {
+    return turnos.map((event) => ({
       idturno: `${event.turno.id}`.trim(),
       idtipo: `${event.turno.idtipoTurno.id}`.trim(),
-      nombre:`${event.turno.idmascota.nombre} para ${event.turno.idtipoTurno.nombreTurno} a las ${event.turno.horarioturnodesde} `,
-      numero:`${event.turno.idmascota.idcliente.idpersona.telefono}`,
+      nombre: `${event.turno.idmascota.nombre} para ${event.turno.idtipoTurno.nombreTurno} a las ${event.turno.horarioturnodesde} `,
+      numero: `${event.turno.idmascota.idcliente.idpersona.telefono}`,
       title: `${event.turno.idmascota.nombre} - ${event.turno.idtipoTurno.nombreTurno}`, // Título combinado de la mascota y el tipo de turno
       start: `${event.turno.fechaturno}T${event.turno.horarioturnodesde}`, // Fecha y hora de inicio en formato ISO 8601
       end: `${event.turno.fechaturno}T${event.turno.horarioturnohasta}`, // Fecha y hora de fin en formato ISO 8601
-      horario: event.turno.idtipoTurno.id==1?`${event.turno.fechaturno} \n Horario de ingreso: ${event.turno.horarioturnodesde} \n Día de egreso: ${event.turno.idficha.fechaegreso} Horario de salida: ${event.turno.horarioturnohasta}`:
-      `${event.turno.fechaturno} \n Horario de inicio: ${event.turno.horarioturnodesde} \n Horario de fin: ${event.turno.horarioturnohasta}`,
-      description:event.turno.idtipoTurno.id==4? `Turno de ${event.turno.idmascota.nombre} con el servicio ${event.turno.idtipoTurno.nombreTurno}. 
+      horario:
+        event.turno.idtipoTurno.id == 1
+          ? `${event.turno.fechaturno} \n Horario de ingreso: ${event.turno.horarioturnodesde} \n Día de egreso: ${event.turno.idficha.fechaegreso} Horario de salida: ${event.turno.horarioturnohasta}`
+          : `${event.turno.fechaturno} \n Horario de inicio: ${event.turno.horarioturnodesde} \n Horario de fin: ${event.turno.horarioturnohasta}`,
+      description:
+        event.turno.idtipoTurno.id == 4
+          ? `Turno de ${event.turno.idmascota.nombre} con el servicio ${
+              event.turno.idtipoTurno.nombreTurno
+            }. 
                    \n Estado: ${event.turno.idestado.descripcion} 
                    \n Costo total: $${event.turno.costototal} 
-                   \n Detalle de turno: ${event.detalles.map(detalle=> detalle.tipoServicio.nombre)}
-                   \n Dirección: 
-                   \n Forma de pago y observaciones: `:
-                   `Turno de ${event.turno.idmascota.nombre} con el servicio ${event.turno.idtipoTurno.nombreTurno}. 
+                   \n Detalle de turno: ${event.detalles.map(
+                     (detalle) => detalle.tipoServicio.nombre
+                   )}
+                   \n Dirección: ${
+                     event.turno.idmascota.idcliente.idpersona.iddireccion.calle
+                   } ${
+              event.turno.idmascota.idcliente.idpersona.iddireccion.numero
+            }, ${event.turno.idmascota.idcliente.idpersona.iddireccion.barrio}
+                   \n Forma de pago y observaciones: ${event.turno.formadepago} 
+                   \n Observaciones:  ${event.turno.descripcion}`
+          : `Turno de ${event.turno.idmascota.nombre} con el servicio ${
+              event.turno.idtipoTurno.nombreTurno
+            }. 
                    \n Estado: ${event.turno.idestado.descripcion} 
                    \n Costo total: $${event.turno.costototal} 
-                   \n Detalle de turno: ${event.detalles.map(detalle=> detalle.tipoServicio.nombre)}`
-                   ,
-      backgroundColor: event.turno.idtipoTurno.id==1?`#e5af3d`: event.turno.idtipoTurno.id==2?`#66a2d1`:event.turno.idtipoTurno.id==3?`#378006`:`#8B0000`,
-      borderColor:event.turno.idtipoTurno.id==1?`#e5af3d`: event.turno.idtipoTurno.id==2?`#66a2d1`:`#378006`,
-    }
-  ));
-};
+                   \n Detalle de turno: ${event.detalles.map(
+                     (detalle) => detalle.tipoServicio.nombre
+                   )}`,
+      backgroundColor:
+        event.turno.idtipoTurno.id == 1
+          ? `#e5af3d`
+          : event.turno.idtipoTurno.id == 2
+          ? `#66a2d1`
+          : event.turno.idtipoTurno.id == 3
+          ? `#378006`
+          : `#8B0000`,
+      borderColor:
+        event.turno.idtipoTurno.id == 1
+          ? `#e5af3d`
+          : event.turno.idtipoTurno.id == 2
+          ? `#66a2d1`
+          : `#378006`,
+    }));
+  };
 
-
-const transformedEvents = turnos && turnos.length > 0  ? transformEvents(turnos) : [];
-//console.log('Eventos transformados:', transformedEvents);
-
+  const transformedEvents =
+    turnos && turnos.length > 0 ? transformEvents(turnos) : [];
+  //console.log('Eventos transformados:', transformedEvents);
 
   const handleEventClick = (eventInfo) => {
     setSelectedEvent(eventInfo.event);
@@ -118,11 +143,10 @@ const transformedEvents = turnos && turnos.length > 0  ? transformEvents(turnos)
 
   return (
     <div>
-      
-    <Grid item xs={12} sm={12} className="calendarContainer">
-      <Link to="/turn/register">
-        <CustomButton text="Registrar turno"></CustomButton>
-      </Link> 
+      <Grid item xs={12} sm={12} className="calendarContainer">
+        <Link to="/turn/register">
+          <CustomButton text="Registrar turno"></CustomButton>
+        </Link>
       </Grid>
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
@@ -133,8 +157,8 @@ const transformedEvents = turnos && turnos.length > 0  ? transformEvents(turnos)
           end: "dayGridMonth,timeGridWeek,timeGridDay",
         }}
         height={"auto"} // Ajustar la altura para que se ajuste al contenido
-        events={transformedEvents}// Cargar los eventos transformados
-        //eventColor= '#378006' 
+        events={transformedEvents} // Cargar los eventos transformados
+        //eventColor= '#378006'
         locale={esLocale} // Establece el idioma en español
         eventClick={handleEventClick}
       />
@@ -148,20 +172,28 @@ const transformedEvents = turnos && turnos.length > 0  ? transformEvents(turnos)
               <strong>Turno:</strong> {selectedEvent.extendedProps.horario}
             </p>
             <p>
-              <strong style={{whiteSpace: 'pre-wrap'}}>Descripción: {selectedEvent.extendedProps.description}</strong>{" "}
+              <strong style={{ whiteSpace: "pre-wrap" }}>
+                Descripción: {selectedEvent.extendedProps.description}
+              </strong>{" "}
             </p>
             <button onClick={closeModal}>Cerrar</button>
-            <Link to={`/turn/updateTurn/${selectedEvent.extendedProps.idtipo}/${selectedEvent.extendedProps.idturno}`}>
-            <button>Modificar</button>
-            </Link>            
-            <button onClick={() => {
-              const phoneNumber = `+549${selectedEvent.extendedProps.numero}`; // Número de teléfono en formato internacional sin el "+" (por ejemplo, "5491123456789" para Argentina)
-              const message = encodeURIComponent(`Hola, le recordamos del turno de ${selectedEvent.extendedProps.nombre} .`);
-              const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
-              window.location.href = whatsappUrl;}}>
+            <Link
+              to={`/turn/updateTurn/${selectedEvent.extendedProps.idtipo}/${selectedEvent.extendedProps.idturno}`}
+            >
+              <button>Modificar</button>
+            </Link>
+            <button
+              onClick={() => {
+                const phoneNumber = `+549${selectedEvent.extendedProps.numero}`; // Número de teléfono en formato internacional sin el "+" (por ejemplo, "5491123456789" para Argentina)
+                const message = encodeURIComponent(
+                  `Hola, le recordamos del turno de ${selectedEvent.extendedProps.nombre} .`
+                );
+                const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
+                window.location.href = whatsappUrl;
+              }}
+            >
               WhatsApp
             </button>
-
           </div>
         </div>
       )}
